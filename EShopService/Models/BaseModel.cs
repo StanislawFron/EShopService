14454e5a5120d1a05df23836b0e0f0a4c83f59bb
Stanisław Froń <stasiu.fron2003@gmail.com>
2025-03-18 22:50:53 +0100

    LAB 1

---
+++ EShopService/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EShopService;
+
+public class BaseModel
+{
+    private int id { get; set; };
+    private string name { get; set; };
+    private Bool deleted { get; set; };
+    private DateTime created_at { get; set; };
+    private Guid created_by { get; set; };
+    private DateTime updated_at { get; set; };
+    private Guid updated_by { get; set; };
+}
---
+++ EShopService/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace EShopService;
+
+public class Product : BaseModel
+{
+    private string ean { get; set; };
+    private decimal price { get; set; };
+    private int stock { get; set; };
+    private string sku { get; set; };
+    private Category category { get; set; };
+}
